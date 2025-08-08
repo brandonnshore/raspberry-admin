c5f34a86ffce4619720aa6f80968a7d2049732a2
brandonnshore <brandonnshore@gmail.com>
2025-08-08 18:10:36 -0400

    Create products.service.ts

---
+++ apps/api/src/products/products.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+@Injectable()
+export class ProductsService {
+  async findAll() {
+    return prisma.product.findMany();
+  }
+}
